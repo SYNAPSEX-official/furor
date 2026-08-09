a8f8de60f4172393db0e1986a93a859d58b0e6bd
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-08-09 15:10:24 +0530

    levels

---
+++ Scripts/clothes/skirt.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+var clothe_gender = "female"
+func _on_body_entered(body: Node2D) -> void:
+		if body.name == "Player":
+				if clothe_gender != Global.gender:
+						Global.rage_multi += 0.2
+				else:
+						Global.bottom_cloth = "skirt"
--- Scripts/gender.gd
+++ Scripts/gender.gd
@@ -1,31 +1,31 @@
 extends Control
 
 func _on_male_pressed() -> void:
-	Global.gender = "male"
+	Global.set_gender("male")
 	get_tree().change_scene_to_file("res://UI/skin.tscn")
 
 func _on_female_pressed() -> void:
-	Global.gender = "female"
+	Global.set_gender("female")
 	get_tree().change_scene_to_file("res://UI/skin.tscn")
 
 #TODO change scene files to level 1
 
 func _on_dark_pressed() -> void:
-	Global.skin = 5
+	Global.skin = "5"
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 func _on_tan_pressed() -> void:
-	Global.skin = 4
+	Global.skin = "4"
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 func _on_neutral_pressed() -> void:
-	Global.skin = 3
+	Global.skin = "3"
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 func _on_light_pressed() -> void:
-	Global.skin = 2
+	Global.skin = "2"
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 func _on_pale_pressed() -> void:
-	Global.skin = 1
+	Global.skin = "1"
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
