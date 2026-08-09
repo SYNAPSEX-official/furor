2b039d27401b6b8aa58221c165a5a585dcc2b843
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-08-09 16:07:49 +0530

    level 3 QA fix

--- Scripts/level_2.gd
+++ Scripts/level_2.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
-func _on_level_3_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		get_tree().change_scene_to_file("res://Scenes/level_3.tscn")
